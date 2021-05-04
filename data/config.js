e24ec897455c4fe361caeabe3bf227138ebb2ee9
JesperKYH <jesper.pettersson@student.kyh.se>
2021-05-04 16:28:11 +0200

    Done with labb2

---
+++ data/config.js
@@ -0,0 +1,15 @@
+const mySql = require('mysql')
+
+// Connects to our database
+const config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'main',
+    port: '3306'
+};
+
+// Allows us to have multiple connections at
+const pool = mySql.createPool(config);
+
+module.exports = pool;
--- server.js
+++ server.js
@@ -2,35 +2,80 @@ const express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser'),
     //Initialze express in app
-    app = express()
+    app = express(),
+    pool = require('./data/config');
+    mysql = require('mysql')
 
 const json = require('./data.json')
-
+let counterNum = 0;
 const members = require('./Members')
 
 //Set public as our static folder
 app.use(express.static(path.join(__dirname, 'public')))
-
 //Handle raw JSON
 app.use(express.json())
 // Handle URL encoded data
 app.use(express.urlencoded({ extended: false }))
+
+// Display all users
+app.get('/users', (req, res) => {
+    pool.query('SELECT * FROM users', (error, result) => {
+        if (error) throw error;
+
+        res.send(result);
+    });
+});
+
+// Displays a single user based in id
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    pool.query('SELECT * FROM users WHERE id = ?', id, (error, result) => {
+        if(error) throw error;
+
+        res.send(result)
+    })
+})
 
 //Route handler
 app.get('/', (req, res) => {
     res.send({hey: "buddy"})
 })
 
-// Works like an api endpoint
-app.get('/api/members', (req, res) => {
-    res.json(members)
+// Add a new user
+app.post('/users/add', (req, res) => {
+    pool.query('INSERT INTO users SET ?', req.body, (err, result) => {
+        if(err) throw err;
+
+        res.status(201).send(`User added with ID: ${req.body.id}`);
+    })
+});
+
+// Update a user
+app.put('/users/update/:id', (req, res) => {
+    const id = req.params.id;
+
+    pool.query('UPDATE users SET ? WHERE id = ?', [req.body, id], (error, result) => {
+        if (error) throw error;
+
+        res.send(`User with ID: ${id} was sucessfully updated`)
+    })
 })
 
+// Delete a user
+app.delete('/users/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    pool.query('DELETE FROM users WHERE id = ?', id, (err, result) => {
+        if(err) throw err;
+
+        res.send(`User with ID: ${id} was sucessfully deleted`)
+    })
+})
 
 app.post('/api/members', (req, res) => {
-    let num = Math.floor(Math.random(0, 1) * 1024)
+    //let num = Math.floor(Math.random(0, 1) * 1024)
     const newMember = {
-        id: num,
+        id: Date.now(),
         name: req.body.name,
         email: req.body.email,
         status: 'active'
@@ -51,32 +96,26 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/about.html'))
 })
 
-//* Första delen i VG
 app.get('/api/random', (req, res) => {
     let num = Math.floor(Math.random(0, 1) * 1024)
     res.send({number: num})
 })
 
-//* Andra delen i VG
 app.get('/api/custom_random/:num', (req, res) => {
     let num = Math.floor(Math.random(0, 1) * req.params.num)
     res.send({number: num})
     console.log(num)
 })
 
+app.get('/api/counter', (req, res) => {
+    res.send({num: counterNum})
+})
+app.get('/api/counter/add', (req, res) => {
+    counterNum++;
+    res.send({num: counterNum})
+})
+
 //Tells express which port to listen to, either the environement variables port or port 3000 if env port does not exist
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server started succefully on port ${PORT}`));
-
-
-/*
-LINKS:
-https://expressjs.com/
-https://expressjs.com/en/starter/hello-world.html
-https://www.youtube.com/watch?v=L72fhGm1tfE&ab_channel=TraversyMedia
-https://stackoverflow.com/questions/40556650/generate-random-number-with-node-js-page
-*/
-/*
-Jesper och Jon har gjort denna tillsammans (lika mycket)
-*/
+app.listen(PORT, () => console.log(`Server started succefully on port: http://localhost:${PORT}`));
